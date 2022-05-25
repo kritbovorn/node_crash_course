8de4737e1f16dfa1cdfb438f8f257c5fcc3ca0df
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-25 19:21:17 +0700

    send html to the browser

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    // Set Header Content
+    res.setHeader('Content-Type', 'text/html');
+
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log('Oops!!! Have error' + err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+
+    });
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Listening for request on PORT 3000');
+});
