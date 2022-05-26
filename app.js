2716a8e604c87b5129f51a71ba43be704a2c91ef
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-26 22:13:55 +0700

    add: add-blog, all-blog manual

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const morgan = require('morgan');
 // mongoose
 const mongoose = require('mongoose');
 
+// Import Blog
+const Blog = require('./models/blog');
+
 // Connect to MongoDB
-const dbURI = 'mongodb+srv://kritbovorn:JNsBuHYtFxxwXfDq@nodetutorial.ftuix.mongodb.net/?retryWrites=true&w=majority'
+const dbURI = 'mongodb+srv://boie:B11MLQa9fOdveWMw@nodetutor.jpayl.mongodb.net/?retryWrites=true&w=majority'
 //  Use mongoose
 mongoose.connect(dbURI).then((result) => {
     console.log('Connect DataBase Sirrrrrrrrrr');
@@ -32,6 +35,47 @@ app.use(express.static('public'));      // File in this : public folder : can ac
 // ###  Use morgan
 app.use(morgan('dev'));
 
+// ### Test add Data to Database
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New Blog 3',
+        snippet: 'About my new blog and every things',
+        body: 'More about my new some blog.',
+    });
+
+    blog.save().then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log('Oops!! ' + err);
+    });
+});
+
+
+// Find Allssss
+app.get('/all-blogs', (req, res) => {
+    Blog.find().then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log('Oops : cannot show all Blogs ' + err);
+    });
+});
+
+// Find Single blog
+app.get('/single-blog', (req, res) => {
+    Blog.findById('628f83d3d26b9a928e05b120').then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log('Oops : Cannot get Data from ID');
+    });
+});
+
+
+
+
+// Routes
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
