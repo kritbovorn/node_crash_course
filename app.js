11cf0891658fc824fbff399a8dfbedb0c9edcb54
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-26 13:56:07 +0700

    finish chapter 7

--- app.js
+++ app.js
@@ -3,26 +3,32 @@ const express = require('express');
 // express app
 const app = express();
 
+// Register View Engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');    //  if we want to change folder of Views
+
+
 
 // Listen for Request port 3000
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Home Page นะจ๊ะ</h1>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<h1>About Page นะจ๊ะ</h1>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
-// Redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
 });
-
 // 404 Page
 app.use((req, res) => {
-    res.statusCode(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 })
