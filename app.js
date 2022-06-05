8a7e2169e0501c3edbd8e2eab5e6d84911ee77d1
Kritbovorn <ktaweeyossak@yahoo.com>
2022-06-05 18:50:09 +0700

    redirect from homepage to fetched all blogs routes

--- app.js
+++ app.js
@@ -25,53 +25,23 @@ mongoose.connect(dbURI).then((result) => {
 // Middleware
 app.use(morgan('dev'));
 
-// mongoose and mongo
-
-// Add BLog
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog2',
-        snippet: 'about my new blog2',
-        body: 'more about my new blog.......'
-    });
-
-    blog.save().then((result) => {
-        res.send(result)
-    }).catch((err) => {
-        console.log(err);
-    });
-});
-
-// All Blog
-app.get('/all-blogs', (req, res) => {
-    Blog.find().then((result) => {
-        res.send(result);
-    }).catch((err) => {
-        console.log(err);
-    });
-});
-
-// Selected Blog
-app.get('/selected-blog', (req, res) => {
-    Blog.findById('629c9043042f3ce8bed9ebf7').then((result) => {
-        res.send(result);
-    }).catch((err) => {
-        console.log(err);
-    });
-});
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. '},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. '},
-        {title: 'how to defeast browser', snippet: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. '},
-    ];
-    res.render('index', { title: 'Home Page', blogs: blogs});
+    res.redirect('/blogs');
 });
 
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About Page'});
+});
+
+// Blog Routes
+app.get('/blogs', (req,res) => {
+    Blog.find().sort({createdAt: -1}).then((result) => {
+        res.render('index', {title: 'All Blogs', blogs: result})
+    }).catch((err) => {
+        console.log(err);
+    });
 });
 
 app.get('/blogs/create', (req, res) => {
